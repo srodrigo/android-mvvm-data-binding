ab8804f61b79345f7bdc83998049975f9ee88e7c
Sergio Rodrigo <srodrigo@MacBook-Pro-de-Sergio.local>
2015-06-12 20:13:05 +0200

    Added License.

--- app/src/main/java/me/srodrigo/databindingtest/common/presentation/BindingViewFactory.java
+++ app/src/main/java/me/srodrigo/databindingtest/common/presentation/BindingViewFactory.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2015 Sergio Rodrigo Royo
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
 package me.srodrigo.databindingtest.common.presentation;
 
 import me.srodrigo.databindingtest.modules.user.UserBindingViewImp;
